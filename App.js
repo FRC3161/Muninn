c3a7a193438f9415b807d437cec73c4e6115a038
David Drobner <david@drobner.ca>
2020-03-20 00:33:15 -0400

    Update AutoScreen

--- App.js
+++ App.js
@@ -1,8 +1,9 @@
 import React, { Component } from 'react';
 import { Text, View } from 'react-native';
-import { NavigationContainer, DefaultTheme } from '@react-navigation/native';
+import { NavigationContainer } from '@react-navigation/native';
 import { createStackNavigator } from '@react-navigation/stack';
 import Homescreen from "./components/Homescreen";
+import Auto from "./components/Auto";
 
 const Stack = createStackNavigator();
 const name = "Scouting App";
@@ -24,11 +25,18 @@ class Home extends Component {
   }
 }
 
+class AutoScreen extends Component {
+  render() {
+    return <Auto/>
+  }
+}
+
 function App() {
   return (
     <NavigationContainer theme={MyTheme}>
       <Stack.Navigator>
         <Stack.Screen name="Step 1" component={Home}/>
+        <Stack.Screen name="Step 2" component={AutoScreen}/>
       </Stack.Navigator>
     </NavigationContainer>
   )
