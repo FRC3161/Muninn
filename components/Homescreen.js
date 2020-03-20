04ad560a84c9f967c3b36a48d7b1bfa945282625
David Drobner <david@drobner.ca>
2020-03-19 23:38:26 -0400

    Font in Homescreen

--- components/Homescreen.js
+++ components/Homescreen.js
@@ -1,13 +1,30 @@
 import React, {Component} from 'react';
-import { Text, View } from 'react-native';
-import { NavigationContainer } from '@react-navigation/native';
-import { createStackNavigator } from '@react-navigation/stack'
+import { Text, View, StyleSheet } from 'react-native';
+
+const styles = StyleSheet.create({
+    title: {
+        fontFamily: "Roboto",
+        fontSize: 42,
+        textAlign: "left",
+        marginTop: 25,
+    },
+
+    text: {
+        fontFamily: "Roboto",
+        fontSize: 12, // Todo
+        textAlign: "left",
+        marginTop: 30
+    }
+})
 
 class Homescreen extends Component {
+
     render() {
         return(
-            <View style={{alignContent: "center"}}>
-                <Text> Home </Text>
+            <View>
+                <View style={{marginLeft: "5%",}}>
+                    <Text style={styles.title}> General {"\n"} Information </Text>
+                </View>
             </View>
         )
     }
