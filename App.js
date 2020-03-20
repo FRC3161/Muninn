ce19f6b8df02ce222d0554ac04cf97af99d5b0dd
David Drobner <david@drobner.ca>
2020-03-20 00:08:38 -0400

    Homescreen basic layout

--- App.js
+++ App.js
@@ -1,11 +1,22 @@
 import React, { Component } from 'react';
 import { Text, View } from 'react-native';
-import { NavigationContainer } from '@react-navigation/native';
+import { NavigationContainer, DefaultTheme } from '@react-navigation/native';
 import { createStackNavigator } from '@react-navigation/stack';
 import Homescreen from "./components/Homescreen";
 
 const Stack = createStackNavigator();
 const name = "Scouting App";
+
+const MyTheme = {
+  dark: true,
+  colors: {
+    primary: "#B3861B",
+    border: "#B3861B",
+    card: "#B3861B",
+    background: "#262626",
+    text: "#FFFFFF"
+  }
+}
 
 class Home extends Component {
   render() {
@@ -15,7 +26,7 @@ class Home extends Component {
 
 function App() {
   return (
-    <NavigationContainer>
+    <NavigationContainer theme={MyTheme}>
       <Stack.Navigator>
         <Stack.Screen name="Step 1" component={Home}/>
       </Stack.Navigator>
--- components/Homescreen.js
+++ components/Homescreen.js
@@ -1,5 +1,5 @@
 import React, {Component} from 'react';
-import { Text, View, StyleSheet } from 'react-native';
+import { Text, View, StyleSheet, TextInput, Button } from 'react-native';
 
 const styles = StyleSheet.create({
     title: {
@@ -7,14 +7,29 @@ const styles = StyleSheet.create({
         fontSize: 42,
         textAlign: "left",
         marginTop: 25,
+        color: "#FFFFFF"
     },
 
     text: {
         fontFamily: "Roboto",
-        fontSize: 12, // Todo
+        fontSize: 24, // Todo
         textAlign: "left",
-        marginTop: 30
-    }
+        marginTop: 30,
+        color: "#FFFFFF"
+    },
+
+    input: {
+        height: 25,
+        borderTopWidth: 0,
+        borderRightWidth: 0,
+        borderLeftWidth: 0,
+        width: "60%",
+        marginTop: 5,
+        borderBottomColor: '#B3861B',
+        borderWidth: 3,
+        color: "#FFFFFF"
+    },
+
 })
 
 class Homescreen extends Component {
@@ -22,8 +37,27 @@ class Homescreen extends Component {
     render() {
         return(
             <View>
-                <View style={{marginLeft: "5%",}}>
+                <View style={{marginLeft: "5%"}}>
                     <Text style={styles.title}> General {"\n"} Information </Text>
+                </View>
+                <View style={{marginLeft: "5%"}}>
+                    <Text style={styles.text}> Scouter's Name </Text>
+                    <TextInput style={styles.input}></TextInput>
+                    <Text style={styles.text}> Match Number # </Text>
+                    <TextInput style={styles.input}></TextInput>
+                    <Text style={styles.text}> Team Number # </Text>
+                    <TextInput style={styles.input}></TextInput>
+                    <Text style={styles.text}> Alliance Colour </Text>
+                    <View style={{flexDirection: "row", marginTop: 5, marginLeft: "2%"}}>
+                        <View style={{paddingRight: 25}}>
+                            <Button title="Red" color="#CC1717"/>
+
+                        </View>
+                        <Button title="Blue" color="#174DCC"/>
+                    </View>
+                </View>
+                <View style={{width: "90%", marginLeft: "5%", marginTop: 25}}>
+                    <Button title="Next" color="#B3861B"/>
                 </View>
             </View>
         )
