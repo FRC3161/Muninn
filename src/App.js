32c39d1c0a53ea051b8855ed1a5a7b8f9951cf45
David Drobner <david@drobner.ca>
2021-02-02 10:06:48 -0500

    Gold buttons

--- src/App.js
+++ src/App.js
@@ -1,13 +1,15 @@
 import './App.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import Button from 'react-bootstrap/Button'
+import './reactstrap.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-         <Button variant={"primary"} size={"lg"} onClick={testPopup}>Test</Button>
-      </header>
+        <header className="App-header">
+            <h1> Muninn </h1>
+            <Button variant={"gold"} size={"lg"} onClick={testPopup}>Test</Button>
+        </header>
     </div>
   );
 }
