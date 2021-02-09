afc857451bf4544d6d0b2444521725df7b09256b
David Drobner <david@drobner.ca>
2021-02-08 19:52:31 -0500

    Tabbed interface

--- src/App.js
+++ src/App.js
@@ -1,22 +1,27 @@
 import './App.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
-import Button from 'react-bootstrap/Button'
+import Button from 'react-bootstrap/Button';
+import Tabs from 'react-bootstrap/Tabs';
+import Tab from 'react-bootstrap/Tabs';
+import React from "react";
 import './reactstrap.css';
 
 function App() {
   return (
-    <div className="App">
-        <header className="App-header">
-            <h1> Muninn </h1>
-            <Button variant={"gold"} size={"lg"} onClick={testPopup}>Test</Button>
-
-        </header>
-    </div>
+      <div className="App">
+          <header className="App-header">
+              <Tabs defaultActiveKey={"info"} id={"data-entry-tabs"}>
+                  <Tab eventKey={"info"} title={"Scouter Info"}>
+                      <h1> Placeholder </h1>
+                  </Tab>
+                  <Tab eventKey={"auto"} title={"Autonomous"}>
+                      <h1> Placeholder 2</h1>
+                  </Tab>
+              </Tabs>
+          </header>
+      </div>
   );
 }
 
-function testPopup(){
-    window.alert("test");
-}
 
 export default App;
